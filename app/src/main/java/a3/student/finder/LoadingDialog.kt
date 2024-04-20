4ce8958ed31118b0f3f5a909bfeef8edae6e2d69
Abdullah khidr <Abdullahkhidrmain@gmail.com>
2024-04-20 08:48:36 +0200

    Improve Loading Dialog

--- app/src/main/java/a3/student/finder/LoadingDialog.kt
+++ app/src/main/java/a3/student/finder/LoadingDialog.kt
@@ -1,14 +1,24 @@
 package a3.student.finder
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 @Composable
 fun LoadingDialog(dismiss: Boolean) {
     if (!dismiss) {
         Dialog(onDismissRequest = {}) {
-            CircularProgressIndicator()
+            CircularProgressIndicator(
+                modifier = Modifier.size(100.dp),
+                color = Color.Cyan,
+                strokeWidth = 8.dp,
+                strokeCap = StrokeCap.Round
+            )
         }
     }
 }
